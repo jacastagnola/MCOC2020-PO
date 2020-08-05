270c78d565b51018567274ea868b8c0cb4c8e1ef
jacastagnola <69158084+jacastagnola@users.noreply.github.com>
2020-08-05 18:36:38 -0400

    Add files via upload

---
+++ P0.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 17:23:00 2020
+
+@author: Asus
+"""
+nombre= "Juan Andres"
+dia= 3
+
+print ("Forma 1")
+print ("hola Juan Andres es el 3 de Agosto") #Forma 1
+
+print ("Forma 2")
+print("hola " + nombre + " es el " + str(dia) + " de Agosto ") #Forma 2
+
+print ("Forma 3")
+print("hola {nombre} es el {dia} de Agosto ".format(dia=dia, nombre=nombre))#Forma 3
+
+print ("Forma 4")
+print(f"hola {nombre} es el {dia} de Agosto" ) #Forma 4
+
+
+
+
+
+
+
+
+
